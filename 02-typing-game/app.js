c08988387144de882e352d817021dab8f1cce064
Mateo Roldán <mateo.mat9@gmail.com>
2024-06-16 01:37:53 -0500

    Iniciamos el segundo proyecto llamado typing game

---
+++ 02-typing-game/app.js
@@ -0,0 +1,17 @@
+const $time = document.querySelector('time');
+const $paragraph = document.querySelector('paragraph');
+const $input = document.querySelector('input');
+
+const INITIAL_TIME = 30;
+
+const TEXT = 'the quick brown fox jumps over the lazy dog and you is trying to clone monkey type for fun and profit using vanilla js for the typing test speed';
+
+let words = [];
+let currentTime = INITIAL_TIME;
+
+//Llamamos las funciones
+initGame();
+initEvents();
+
+function initGame() {};
+function initEvents() {};
