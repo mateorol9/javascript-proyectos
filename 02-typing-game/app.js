28cffe20c818ad7f88a677a4818a277b2c850a5f
Mateo Roldán <mateo.mat9@gmail.com>
2024-06-17 00:47:36 -0500

    Configuramos el inicio del juego, pero queda pendiente el contador de cuenta regresiva, además se establecieron algunos estilos css basicos.

--- 02-typing-game/app.js
+++ 02-typing-game/app.js
@@ -1,5 +1,6 @@
+// '$' para identificar los elementos del DOM
 const $time = document.querySelector('time');
-const $paragraph = document.querySelector('paragraph');
+const $paragraph = document.querySelector('p');
 const $input = document.querySelector('input');
 
 const INITIAL_TIME = 30;
@@ -13,5 +14,22 @@ let currentTime = INITIAL_TIME;
 initGame();
 initEvents();
 
-function initGame() {};
+function initGame() {
+    // Iniciamos el estado, separamos el texto por espacios
+    words = TEXT.split(' ').slice(0, 32);
+    // Reiniciamos el tiempo del juego
+    currentTime = INITIAL_TIME;
+
+    $time.textContent = currentTime;
+    $paragraph.innerHTML = words.map((word, index) => {
+        // De cada palabra vamos a sacar las letras
+        const letters = word.split('');
+        // Creamos una etiqueta <x-word> (creamos un custom element) y dentro de ella cada letra de la palabra va a ir dentro de una etiqueta <letter>
+        return `<x-word>
+            ${letters.map(letter => `<x-letter>${letter}</x-letter>`).join('')}
+        </x-word>`;
+    }).join(''); // Agregar .join('') al final para convertir el array de strings en un solo string HTML
+    //Establecemos el contador de tiempo
+    
+}
 function initEvents() {};
